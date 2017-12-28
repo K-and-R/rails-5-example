5277ce0c50a7152203dd518bcfce664454577557
Karl Wilbur <karl@kandrsoftware.com>
2017-12-27 21:13:19 -0500

    Website views (stubs).

---
+++ app/controllers/website_controller.rb
@@ -0,0 +1,10 @@
+class WebsiteController < ApplicationController
+  def welcome
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get '/welcome', to: 'website#welcome'
+  get '/about', to: 'website#about'
+  get '/contact', to: 'website#contact'
+
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: redirect('/welcome')
 end
